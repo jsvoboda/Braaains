2421234da81431aa564c264da2aa950287c74060
Jaromír Svoboda <svobod8@gmail.com>
2013-08-23 13:45:13 +0200

    Simple prey AI

---
+++ Assets/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static Vector3 nullYAxis(this Vector3 original){
+        return new Vector3(original.x, 0, original.z);
+    }
+}
+
--- Assets/PreyScript.cs
+++ Assets/PreyScript.cs
@@ -1,73 +1,116 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class PreyScript : MonoBehaviour {
-
+public class PreyScript : MonoBehaviour
+{
+    public GameObject Walls;
     public GameObject Zombie;
     public GameObject ScoreDaemon;
-
     public float FleeDistance;
 
     bool isFleeing;
+    Vector3 currentDirection;
+    Vector3 desiredDirection;
+    Vector3 steering;
+    List<Vector3> castingDirections = new List<Vector3>();
 
-    System.Random random;
-    float previousTime;
-    Vector3 moveDirection;
+    float startTime = 0;
 
-	// Use this for initialization
-	void Start () {
-        random = new System.Random();
-        previousTime = Time.time;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        isFleeing = (this.transform.position - Zombie.transform.position).magnitude < FleeDistance;
-        
-	}
+    //Vector3[] castingDirections = new Vector3[] {
+    //    new Vector3(0,0,1),
+    //    new Vector3(1,0,1),
+    //    new Vector3(1,0,0),
+    //    new Vector3(1,0,-1),
+    //    new Vector3(0,0,-1),
+    //    new Vector3(-1,0,-1),
+    //    new Vector3(-1,0,0),
+    //    new Vector3(-1,0,1)
+    //};
 
-    void FixedUpdate()
+    //float startTime;
+    //float turnTime;
+
+    void Start()
     {
-        if (isFleeing)
+        int vectCount = 8;
+        for (int i = 0; i < vectCount; i++)
         {
-            if (random.NextDouble() * 5 < Time.time - previousTime)
-            {
-                //Vector3 newVector = Quaternion.Euler(0, Random.Range(-60, 60), 0) * (this.transform.position - Zombie.transform.position).normalized;
-
-                moveDirection = Quaternion.Euler(0, Random.Range(-70, 70), 0) * (this.transform.position - Zombie.transform.position).normalized;
-                rigidbody.AddForce(15 * moveDirection);
-
-                //Vector3 horizontalMove = new Vector3(moveDirection.x, 0, moveDirection.z);
-                //this.transform.Translate(horizontalMove * Time.deltaTime);
-            }
+            Vector3 v = Quaternion.Euler(0, i * (360/vectCount), 0) * new Vector3(1, 0, 0);
+            castingDirections.Add(v);
         }
-        else
-        {
-            if (random.NextDouble() * 15 < Time.time - previousTime)
-            {
-                moveDirection = Quaternion.Euler(0, Random.Range(-50, 50), 0) * moveDirection;
-                rigidbody.AddForce(4 * moveDirection);
-            }
-        }
-        
+
+
+        currentDirection = (Random.rotation * new Vector3(1, 1, 1)).nullYAxis();
+        //currentDirection = new Vector3(0, 0, 0);
+        desiredDirection = (Random.rotation * new Vector3(1, 1, 1)).nullYAxis().normalized;
     }
 
-    void OnCollisionEnter(Collision other)
+    void Update()
     {
-        //Debug.Log(other.gameObject.name);
+        //var fracComplete = (Time.time - startTime) / journeyTime;
+        var rotation = Vector3.Lerp(currentDirection, desiredDirection, 2 * (Time.time - startTime));
 
-        //if (other.gameObject.name == "Room")
-        //{
-        //    Debug.Log("points " + other.contacts.Length);
-        //    Debug.Log("normal " + other.contacts[0].normal);
+        Debug.Log(rotation);
 
-        //    moveDirection = -other.contacts[0].normal.normalized;
-        //}
+        //steering = desiredDirection - currentDirection;
+        //currentDirection = currentDirection + steering;
+        //Debug.Log(rotation.nullYAxis() * Time.deltaTime * 5);
+        transform.Translate(rotation.nullYAxis() * Time.deltaTime * 3);
+    }
 
-        if (other.gameObject.name == "Zombie")
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.collider.gameObject.name == "Walls")
+        {
+            desiredDirection = - directionTo(other.collider.gameObject);
+        }
+
+        if (other.collider.gameObject.name == "Zombie")
         {
             ScoreDaemon.GetComponent<ScoreScript>().IncrementScore();
             Destroy(this.gameObject);
         }
+    }
+
+    //Approximate direction from this to closest point of the other object
+    Vector3 directionTo(GameObject other)
+    {
+        List<RaycastHit> hits = new List<RaycastHit>();
+
+        foreach (var d in castingDirections)
+        {
+            hits.AddRange(Physics.SphereCastAll(this.transform.position, 0.1f, d));
+        }
+
+        RaycastHit closest = hits[0];
+        float minDist = closest.distance;
+        foreach (var hit in hits)
+        {
+            if (hit.distance < minDist)
+            {
+                minDist = hit.distance;
+                closest = hit;
+            }
+        }
+
+        foreach (var hit in hits)
+        {
+            var mark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            mark.transform.position = hit.point;
+            mark.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        }
+
+        var mainMark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        mainMark.transform.position = closest.point;
+        mainMark.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+
+        startTime = Time.time;
+
+        return Quaternion.Euler(0, Random.Range(-30, 30), 0)
+            * (closest.point - this.transform.position).normalized;
+
+        //return closest.point - this.transform.position;
     }
 }
