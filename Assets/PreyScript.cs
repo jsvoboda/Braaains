a62e58737d996dd26affd8d290d5e82cccd134b8
Jaromír Svoboda <svobod8@gmail.com>
2013-08-23 15:43:03 +0200

    Steering prey

--- Assets/PreyScript.cs
+++ Assets/PreyScript.cs
@@ -16,22 +16,6 @@ public class PreyScript : MonoBehaviour
     Vector3 steering;
     List<Vector3> castingDirections = new List<Vector3>();
 
-    float startTime = 0;
-
-    //Vector3[] castingDirections = new Vector3[] {
-    //    new Vector3(0,0,1),
-    //    new Vector3(1,0,1),
-    //    new Vector3(1,0,0),
-    //    new Vector3(1,0,-1),
-    //    new Vector3(0,0,-1),
-    //    new Vector3(-1,0,-1),
-    //    new Vector3(-1,0,0),
-    //    new Vector3(-1,0,1)
-    //};
-
-    //float startTime;
-    //float turnTime;
-
     void Start()
     {
         int vectCount = 8;
@@ -43,21 +27,15 @@ public class PreyScript : MonoBehaviour
 
 
         currentDirection = (Random.rotation * new Vector3(1, 1, 1)).nullYAxis();
-        //currentDirection = new Vector3(0, 0, 0);
         desiredDirection = (Random.rotation * new Vector3(1, 1, 1)).nullYAxis().normalized;
     }
 
     void Update()
     {
-        //var fracComplete = (Time.time - startTime) / journeyTime;
-        var rotation = Vector3.Lerp(currentDirection, desiredDirection, 2 * (Time.time - startTime));
+        steering = (desiredDirection - currentDirection) * 0.1f;
+        currentDirection = currentDirection + steering;
 
-        Debug.Log(rotation);
-
-        //steering = desiredDirection - currentDirection;
-        //currentDirection = currentDirection + steering;
-        //Debug.Log(rotation.nullYAxis() * Time.deltaTime * 5);
-        transform.Translate(rotation.nullYAxis() * Time.deltaTime * 3);
+        transform.Translate(currentDirection.nullYAxis() * Time.deltaTime * 3);
     }
 
     void OnTriggerEnter(Collider other)
@@ -95,22 +73,7 @@ public class PreyScript : MonoBehaviour
             }
         }
 
-        foreach (var hit in hits)
-        {
-            var mark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            mark.transform.position = hit.point;
-            mark.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-        }
-
-        var mainMark = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        mainMark.transform.position = closest.point;
-        mainMark.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
-
-        startTime = Time.time;
-
-        return Quaternion.Euler(0, Random.Range(-30, 30), 0)
+        return Quaternion.Euler(0, Random.Range(-50, 50), 0)
             * (closest.point - this.transform.position).normalized;
-
-        //return closest.point - this.transform.position;
     }
 }
